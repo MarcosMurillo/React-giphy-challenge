2b5a17f5b253a345fade11a9f6d937972d1b9e2d
Marcos Murillo <murillo.psoft@gmail.com>
2021-02-17 15:07:31 -0300

    Fixed infinite request loop

--- src/components/GiphyList/index.js
+++ src/components/GiphyList/index.js
@@ -1,38 +1,54 @@
 import React, { useState, useRef } from "react";
-import { FaSearch } from "react-icons/fa";
+import { FaSearch, FaSave } from "react-icons/fa";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { api } from "../../services/api";
 
 export default function GiphyList() {
   const [gifs, setGifs] = useState([]);
   const [offset, setOffset] = useState(0);
+  const [hasMore, setHasMore] = useState(false);
+  const [totalCount, setTotalCount] = useState(0);
+  const [inputValue, setInputValue] = useState("");
   const inputEl = useRef("");
-  const arraySize = 24;
+  const arraySize = 28;
 
   function resetList() {
     setGifs([]);
     setOffset(0);
   }
 
+  function handleClickButtonSearch() {
+    if (inputEl.current.value !== inputValue) {
+      resetList();
+      setInputValue(inputEl.current.value);
+    } else return;
+  }
+
   async function handleSearch(e) {
+    if (inputEl.current.value === "") {
+      return alert("Ops! Informe um valor para realizar a pesquisa");
+    }
     e.preventDefault();
-    resetList();
     setOffset(offset + arraySize);
-    const { data } = await api.get(
-      `/search?q=${inputEl.current.value}&api_key=I8Tl1S2Ras9ILwz2jV9gBp71B97HCBlA&limit=${arraySize}&offset=${offset}`
+    const { data, pagination } = await api.get(
+      `/search?q=${inputValue}&api_key=I8Tl1S2Ras9ILwz2jV9gBp71B97HCBlA&limit=${arraySize}&offset=${offset}`
     );
+    setTotalCount(pagination.total_count);
+    setHasMore(true);
     setGifs(gifs.concat(data.data));
   }
 
-  // async function fetchInitialData() {
-  //   setOffset(offset + arraySize);
-  //   setTimeout(async () => {
-  //     const { data } = await api.get(
-  //       `/search?q=starwars&api_key=I8Tl1S2Ras9ILwz2jV9gBp71B97HCBlA&limit=${arraySize}&offset=${offset}`
-  //     );
-  //     setGifs(gifs.concat(data.data));
-  //   }, 1500);
-  // }
+  async function fetchLoadMoreData() {
+    if (totalCount > offset) {
+      setOffset(offset + arraySize);
+      setTimeout(async () => {
+        const { data } = await api.get(
+          `/search?q=${inputValue}&api_key=I8Tl1S2Ras9ILwz2jV9gBp71B97HCBlA&limit=${arraySize}&offset=${offset}`
+        );
+        setGifs(gifs.concat(data.data));
+      }, 1500);
+    } else setHasMore(false);
+  }
 
   return (
     <>
@@ -44,26 +60,45 @@ export default function GiphyList() {
           }}
         >
           <input type="text" placeholder="Search" ref={inputEl} />
-          <button type="submit">
+          <button
+            type="submit"
+            onClick={() => {
+              handleClickButtonSearch();
+            }}
+          >
             <FaSearch />
           </button>
         </form>
+        <div>
+          <button>
+            <FaSave />
+          </button>
+        </div>
       </div>
       <div className="list-container">
-        {gifs.length && <InfiniteScroll
-          dataLength={gifs.length}
-          next={handleSearch()}
-          hasMore={true}
-          loader={<span className="color-white text-sm fw-600">Loading...</span>}
-        >
-          <ul className="e-list">
-            {gifs.map((giphy, index) => (
-              <li key={`${giphy.id}-${index}`}>
-                <img src={giphy.images.fixed_height.url} alt={giphy.title} />
-              </li>
-            ))}
-          </ul>
-        </InfiniteScroll>}
+        {gifs.length && (
+          <InfiniteScroll
+            dataLength={gifs.length}
+            next={() => {
+              fetchLoadMoreData();
+            }}
+            hasMore={hasMore}
+            loader={
+              hasMore() && (
+                <span className="color-white text-sm fw-600">Loading...</span>
+              )
+            }
+          >
+            <ul className="e-list">
+              {gifs.map((giphy, index) => (
+                <li key={`${giphy.id}-${index}`}>
+                  <img src={giphy.images.fixed_height.url} alt={giphy.title} />
+                  <FaSave className="icon-xxl" />
+                </li>
+              ))}
+            </ul>
+          </InfiniteScroll>
+        )}
       </div>
     </>
   );
